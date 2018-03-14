f0a6388be5eeb344ac02ae5dea84a82baadd631f
lubuntu <lubuntu@lubuntu-VirtualBox>
2018-03-14 11:54:20 +0100

    re-import class 6.1

---
+++ JavaEE_VisiteGuidee/src/fr/eseo/javaee/projet/visiteguidee/Visite.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package fr.eseo.javaee.projet.visiteguidee;
+
+import java.util.Date;
+
+/**
+ * @author lubuntu
+ *
+ */
+public class Visite {
+
+	public Visite() {
+		int codeVisite = 0;
+		String typeDeVisite = null;
+		String ville = null;
+		Date dateVisite = null;
+		float prix = 0;
+	}
+	
+	public Visite(int codeVisite1, String typeDeVisite1, String ville1, Date dateVisite1, float prix1) {
+		int codeVisite = codeVisite1;
+		String typeDeVisite = typeDeVisite1;
+		String ville = ville1;
+		Date dateVisite = dateVisite1;
+		float prix = prix1;
+	}
+	
+	
+	
+	
+	
+}
---
+++ JavaEE_VisiteGuidee/src/fr/eseo/javaee/projet/visiteguidee/methodesServicesWeb.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package fr.eseo.javaee.projet.visiteguidee;
+
+/**
+ * @author lubuntu
+ *
+ */
+public class methodesServicesWeb {
+
+	public Visite trouverVisite (Visite uneVisite) {
+				
+		return null;
+	}
+	
+	public int reserverVisite (ReservationVisite uneVisite) {
+		return 0;
+		
+	}
+	
+	public String payerVisite (int codeReservation) {
+		return null;
+		
+	}
+	
+	public boolean annulerVisite (int codereservation) {
+		return false;
+		
+	}
+	
+}
