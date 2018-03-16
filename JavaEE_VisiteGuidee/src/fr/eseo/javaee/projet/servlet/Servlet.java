e87685bdabf941b1a6116870dfdedb5c929fb0bd
lubuntu <lubuntu@lubuntu-VirtualBox>
2018-03-16 17:08:03 +0100

    interface client servlet

--- JavaEE_VisiteGuidee/src/fr/eseo/javaee/projet/servlet/Servlet.java
+++ JavaEE_VisiteGuidee/src/fr/eseo/javaee/projet/servlet/Servlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.eseo.javaee.projet.visiteguidee.*;
+
 /**
  * Servlet implementation class Servlet
  */
@@ -32,13 +34,18 @@ public class Servlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String reservation = request.getParameter("voyage");
+		ReservationVisite visite = new ReservationVisite();
+		visite.setCodeVisite(Integer.parseInt(request.getParameter("voyage")));
 
+		ReservationVisiteService service = new ReservationVisiteService();
+		ReservationVisiteSEI port = service.getReservationVisitePort();
+		
+		int code = port.reserverVisite(visite);
+				
 		HttpSession session = request.getSession();
-		session.setAttribute("resultat", reservation);
+		session.setAttribute("resultat", code);
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("GestionVisites.jsp");
 		dispatcher.forward(request, response);
-		
 	}
 }
