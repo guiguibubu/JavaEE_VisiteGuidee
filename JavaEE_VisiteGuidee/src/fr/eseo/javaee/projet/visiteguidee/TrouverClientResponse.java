1735ea7f244e2a66b97165424ef7da83a9b5bc5d
guiguibubu <guibu44@hotmail.fr>
2018-03-26 15:38:38 +0200

    ajout trouver client (regénération client)

---
+++ JavaEE_VisiteGuidee/src/fr/eseo/javaee/projet/visiteguidee/TrouverClientResponse.java
@@ -0,0 +1,62 @@
+
+package fr.eseo.javaee.projet.visiteguidee;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour trouverClientResponse complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="trouverClientResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://visiteguidee.projet.javaee.eseo.fr/}client" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "trouverClientResponse", propOrder = {
+    "_return"
+})
+public class TrouverClientResponse {
+
+    @XmlElement(name = "return")
+    protected Client _return;
+
+    /**
+     * Obtient la valeur de la propriété return.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Client }
+     *     
+     */
+    public Client getReturn() {
+        return _return;
+    }
+
+    /**
+     * Définit la valeur de la propriété return.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Client }
+     *     
+     */
+    public void setReturn(Client value) {
+        this._return = value;
+    }
+
+}
